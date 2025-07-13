684cda60a802e0ba901a14b38ac9abebb1aac76d
ThomasJ29 <thomasjeremy1980@gmail.com>
2025-07-13 13:44:30 +0200

    maj

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 <script>
     const list = document.querySelectorAll('.list');
-    function activeLink() {
-       list.forEach((item) => item.classlist.remove('active'));
-       this.classlist.add('active'); 
-       list.forEach((item) => item.addEventListener('click', activeLink))
-    }
+
+function activeLink() {
+  list.forEach((item) => item.classList.remove('active'));
+  this.classList.add('active');
+}
+
+// Ajoute l'événement à chaque élément une seule fois
+list.forEach((item) => item.addEventListener('click', activeLink));
     
 </script>
